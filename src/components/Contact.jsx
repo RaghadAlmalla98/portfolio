860c668952edcc292d88f833d5b43788a90cca02
RaghadAlmalla98 <raghadalmalla1990@gmail.com>
2023-07-20 04:36:09 +0300

    update contact and work component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,12 +11,12 @@ function Contact() {
         action="https://getform.io/f/5c02e6ee-e50b-411c-ad02-8a34b70fa948"
         className="flex flex-col max-w-[600px] w-full h-full"
       >
-        <div className="pb-8">
+        <div className="py-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
           </p>
 
-          <p className="text-gray-300 py-6">
+          <p className="text-gray-300 py-4">
             Submit the form below or shoot me an email -
             raghadalmalla1990@gmail.com
           </p>
@@ -39,7 +39,7 @@ function Contact() {
           rows="10"
           placeholder="Message"
         ></textarea>
-        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center ">
+        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-4 mx-auto flex items-center ">
           Let's Collaobrate
         </button>
       </form>
